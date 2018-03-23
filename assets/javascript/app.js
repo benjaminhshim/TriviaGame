c48890ec0b59505a786fae483ee4156036d804ae
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-03-22 20:24:15 -0700

    Create a function for a new question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,17 +30,22 @@ $(document).ready(function() {
         },
     ]
 
+    var correctAnswers;
+    var wrongAnswers;
 
-    var startButtonLock = false;
-    var answered = false;
+    // THIS VARIABLE WILL REPRESENT THE INDEX OF THE QUESTIONS IN THE ARRAY
+    var currentQuestion = 0;
 
-    var seconds = 10;
+    // DECLARE VARIABLES FOR TIME COUNTDOWN
+    var seconds;
     var interval;
 
     // CREATE FUNCTIONS FOR TIME COUNTDOWN
     function countdown() {
+        seconds = 10;
         $('#time-remaining').html('Time Remaining: ' + seconds);
-
+         
+        clearInterval(interval);
         interval = setInterval(decrement, 1000);
     }
 
@@ -50,6 +55,7 @@ $(document).ready(function() {
 
         if (seconds === 0) {
             stopTime();
+            answerPage();
         }
     }
 
@@ -58,25 +64,79 @@ $(document).ready(function() {
     }
 
 
-    // GAME FUNCTIONS
-    function startGame() {
-        newQuestion();
-    }
-
-    function newQuestion() {
-        
-    }
-
-    // ===== CLICK EVENTS =======
 
     // WHEN USER CLICKS START BUTTON
-        // CALL startGame FUNCTION
+        // DISPLAY QUESTION AND ANSWERS
+        // START TIME COUNTDOWN
     $('#start').click(function() {
+        $('#start').hide();
+        newQuestion();
         countdown();
-        startGame();
+
 
     })
+    
 
+    function newQuestion() {
+        $('#message').empty();
+
+
+        // DISPLAY QUESTION TO THE WINDOW
+        $('#question').html(trivia[currentQuestion].question);
+
+        answerDiv = $('#answers');
+
+        // SORT THROUGH THE CHOICES IN THE ARRAY OF ANSWERS AND APPEND THEM TO EACH OTHER TO #answers IN WINDOW
+        for (var i = 0; i < 4; i++) {
+            choices = $('<li>');
+            choices.addClass('answer');
+            choices.attr('index', i);
+            choices.html(trivia[currentQuestion].answerList[i]);
+            answerDiv.append(choices);
+        }
+
+        countdown();
+
+
+        $('.answer').click(function() {
+            // STORE THE CLICKED ANSWER INTO user AND GIVE IT AN ATTRIBUTE OF index TO CHECK IF IT IS THE CORRECT ANSWER
+            user = $(this).attr('index');
+            clearInterval(interval);
+            answerPage();
+        })
+
+    } 
+
+
+    // THE answerPage IS DISPLAYED AFTER EVERY ANSWER, EITHER RIGHT OR WRONG
+        // TO TELL USER IF THEY GOT THE ANSWER RIGHT OR WRONG
+    function answerPage() {
+        
+        // REMOVE THE QUESTION, ANSWERS AND COUNTDOWN FROM THE WINDOW
+        $('#question').empty();
+        $('#answers').empty();
+        $('#time-remaining').empty();
+
+
+        // CREATE A VARIABLE REPRESENTING THE INDEX OF answerList WITH THE RIGHT NUMBER VALUE
+        var correctAnswerIndex = trivia[currentQuestion].correctAnswer;
+
+        // CREATE A VARIABLE DISPLAY THE STRING OF THE CORRECT ANSWER
+        var correctAnswerString = trivia[currentQuestion].answerList[trivia[currentQuestion].correctAnswer];
+
+        // REPLACE QUESTION AND ANSWERS WITH MESSAGE AND IMAGE
+        if (user == correctAnswerIndex) {
+            $('#message').html('Correct');
+        } else if (user != correctAnswerIndex) {
+            $('#message').html('The answer is ' + correctAnswerString);
+        } else {
+            $('#message').html('out of time. the answer was ' + correctAnswerString);
+        }
+
+        currentQuestion++;
+        setTimeout(newQuestion, 3000);
+
+    }
 
 
 
