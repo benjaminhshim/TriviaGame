8805bec7934e82f5e2a20225801000f8263114dd
Benjamin Shim <benjaminshim@Benjamins-MacBook-Pro.local>
2018-03-22 19:08:31 -0700

    Store questions and answers as objects in an array

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+    // GLOBAL VARIABLES
+
+    var startButtonLock = false;
+    var answered = false;
+
+    var trivia = [
+        {
+            question: "What network is Silicon Valley on?",
+            answerList: ["Netflix", "Amazon Prime", "HBO", "MTV"],
+            correctAnswer: 2
+        },
+        {
+            question: "What is the name of the protagonists' startup?",
+            answerList: ['Pied Piper', 'Aviato', 'Snap',  'Google'],
+            correctAnswer: 0
+        },
+        {
+            question: "What is the name of T.J. Miller's character?",
+            answerList: ['Gilfoyle', 'Big Head', 'Richard',  'Erlich'],
+            correctAnswer: 3
+        },
+        {
+            question: "Russ Hanneman is based off of which investor in Shark Tank?",
+            answerList: ["Kevin O'Leary", 'Daymond John', 'Mark Cuban',  'Robert Herjavec'],
+            correctAnswer: 2
+        },
+        {
+            question: "What is the name of Russ Hanneman's tequila company?",
+            answerList: ['Tres Comas', 'Avion', 'Casamigos',  'Kirkland Signature'],
+            correctAnswer: 0
+        },
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+})
